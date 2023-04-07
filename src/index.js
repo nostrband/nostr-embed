c8dd48ee61ed1c5a246e7d939ccde570034c5cd3
artur <brugeman.artur@gmail.com>
2023-04-07 12:53:44 +0300

    Revert data-nostr back to using id to select wrapper

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { h, render } from 'preact';
 import NostrEmbed from './components/nostrEmbed';
 
-export function init(req) {
+export function init(noteId, wrapper, relay) {
 
-  const noteId = req.noteId;
-  const relay = req.relay || 'wss://relay.nostr.band/all';
+  if (!relay)
+    relay = 'wss://relay.nostr.band/all';
 
-  const wrapper = `.nostr-embed[data-nostr='${noteId}']`;
   const renderElement = document.querySelector(wrapper)
     ? document.querySelector(wrapper)
     : document.querySelector('body');
