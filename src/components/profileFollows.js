746f04a46b636f8842c127d6860cfdea7cefef28
rakhimov <shukurulloh.com@gmail.com>
2023-07-10 22:24:52 +0500

    add link attrebutes

--- src/components/profileFollows.js
+++ src/components/profileFollows.js
@@ -2,24 +2,26 @@ import { getNpub } from "../common";
 
 const ProfileFollows = ({ follows = [], options }) => {
     if (!options?.showFollowing) return <></>
-    const includeProfiles = follows.slice(0, 2).map(follow => {
+    const parsedFollows = follows.slice(0, 10).map(follow => {
+        const content = JSON.parse(follow.content)
         return {
-            npub: getNpub(follow.pubkey),
-            display_name: JSON.parse(follow.content).display_name
+            id: follow?.id,
+            npubLink: `https://nostr.band/${getNpub(follow?.pubkey)}`,
+            display_name: content?.display_name,
+            picture: content?.picture
         }
     })
     return (
         <div>
             <hr />
             <div className="ne-flex ne-gap-6 ne-flex-wrap ne-items-center ne-mt-8">
-                <ul className="ne-list-none ne-p-0 ne-flex ne-ms-[20px] ne-m-0 ne-items-center">
+                <ul className="ne-list-none ne-p-0 ne-flex ne-ms-[25px] ne-m-0 ne-items-center">
                     {
-                        follows.slice(0, 5).map(follow => {
-                            const parsedProfile = JSON.parse(follow.content)
+                        parsedFollows.map(follow => {
                             return (
-                                <li key={parsedProfile?.id}>
-                                    <a href={`https://nostr.band/${getNpub(follow.pubkey)}`}>
-                                        <img className='ne-rounded-full ne-ms-[-20px] ne-object-cover ne-w-[40px]' src={parsedProfile?.picture} alt='follows image' height="40" />
+                                <li key={follow?.id}>
+                                    <a rel="noopener noreferrer nofollow" target="_blank" href={follow.npubLink}>
+                                        <img className='ne-rounded-full ne-ms-[-25px] ne-object-cover ne-w-[40px]' src={follow?.picture} alt='follows image' height="40" />
                                     </a>
                                 </li>
                             )
@@ -31,10 +33,13 @@ const ProfileFollows = ({ follows = [], options }) => {
                         Follows {follows.length} profiles
                     </h2>
                     <div className="ne-m-0 ne-text-sm">
-                        Including &nbsp;
-                        <a className='ne-text-black' href={`https://nostr.band/${includeProfiles[0]?.npub}`}>{includeProfiles[0]?.display_name}</a>
-                        {follows[1] ? ' and ' : ''}
-                        <a className='ne-text-black' href={`https://nostr.band/${includeProfiles[1]?.npub}`}>{includeProfiles[1]?.display_name}</a>
+                        Including {" "}
+                        <a rel="noopener noreferrer nofollow" target="_blank" className='ne-text-black' href={parsedFollows[0]?.npubLink}>{parsedFollows[0]?.display_name}</a>
+                        {follows[1] ? ', ' : ''}
+                        <a rel="noopener noreferrer nofollow" target="_blank" className='ne-text-black' href={parsedFollows[1]?.npubLink}>{parsedFollows[1]?.display_name}</a>
+                        {follows[2] ? ' and ' : ''}
+                        <a rel="noopener noreferrer nofollow" target="_blank" className='ne-text-black' href={parsedFollows[2]?.npubLink}>{parsedFollows[2]?.display_name}</a>
+
                     </div>
                 </div>
             </div>
